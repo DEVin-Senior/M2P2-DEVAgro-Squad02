dd5cf9bda4d8b025beb4d8a82034022cd725568b
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-06-06 21:32:58 -0300

    TASK17 - Criada as pastas do componentes padrões do projeto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterfaceComponent } from './_interfaces/interface/interface.component';
+import { AuthComponent } from './_auths/auth/auth.component';
+import { ViewComponent } from './_views/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterfaceComponent,
+    AuthComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
