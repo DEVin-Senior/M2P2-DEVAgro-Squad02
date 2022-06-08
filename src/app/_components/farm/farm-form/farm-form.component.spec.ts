3d2779114005af163ef26682eeb50675ea8a43bc
rafaelDom <raafita.rad@gmail.com>
2022-06-08 09:59:52 -0300

    task28 - criação da tela de cadastro de fazenda

---
+++ src/app/_components/farm/farm-form/farm-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FarmFormComponent } from './farm-form.component';
+
+describe('FarmFormComponent', () => {
+  let component: FarmFormComponent;
+  let fixture: ComponentFixture<FarmFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FarmFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FarmFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
