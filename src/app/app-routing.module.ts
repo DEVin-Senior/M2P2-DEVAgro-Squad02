329ccc042365947a0b1583173bd51322a3410572
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-06-08 17:29:11 -0300

    TASK29 - Implementado tela de consulta de fazendas (apenas html).

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FarmFormComponent } from './_components/farm/farm-form/farm-form.component';
+import { FarmListComponent } from './_components/farm/farm-list/farm-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "farm/farm-form", component: FarmFormComponent },
+  { path: "farm/farm-list", component: FarmListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
