4e3a678caa39b2ce21414f7805fb0786790f004e
rafaelDom <raafita.rad@gmail.com>
2022-06-07 22:10:35 -0300

    task21 - inclusão do sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,17 @@ import { AppComponent } from './app.component';
 import { UserNavBarComponent } from './_components/user-nav-bar/user-nav-bar.component';
 import { ButtonComponent } from './_components/button/button.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import { GrainFormComponent } from './_components/grain-form/grain-form.component';
-
-@NgModule({
-  declarations: [AppComponent, ButtonComponent, LoginComponent, UserNavBarComponent, GrainFormComponent],
-  imports: [BrowserModule, AppRoutingModule],
-=======
 import { SidebarComponent } from './_components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ButtonComponent,
-    LoginComponent,
-    SidebarComponent,
-    UserNavBarComponent
-  ],
+  declarations: [AppComponent, ButtonComponent, LoginComponent, UserNavBarComponent, GrainFormComponent, SidebarComponent],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
->>>>>>> e257da00996b38879c8d2f15fa80f91f3207d394
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
