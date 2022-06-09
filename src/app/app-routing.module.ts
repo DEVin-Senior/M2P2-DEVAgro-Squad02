fdc9c0b515220d5044277392b25e16844228c0fc
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-06-09 20:16:53 -0300

    TASK22 - Componente tela de consulta funcionários

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { CompanyFormComponent } from './_components/company/company-form/company-form.component';
 import { FarmFormComponent } from './_components/farm/farm-form/farm-form.component';
 import { FarmListComponent } from './_components/farm/farm-list/farm-list.component';
+import { ShowWorkerComponent } from './_components/show-worker/show-worker.component';
 
 const routes: Routes = [
   { path: 'company/company-form', component: CompanyFormComponent },
-  { path: 'login', component: LoginComponent},
-  { path: "farm/farm-form", component: FarmFormComponent },
-  { path: "farm/farm-list", component: FarmListComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'farm/farm-form', component: FarmFormComponent },
+  { path: 'farm/list', component: FarmListComponent },
+  { path: 'employee/list', component: ShowWorkerComponent },
 ];
 
 @NgModule({
