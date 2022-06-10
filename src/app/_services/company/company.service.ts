52df08f586e8092fc2c841800521d5eef0291b38
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-06-10 09:30:55 -0300

    TASK19 - Implementado funcionalidade para cadastro de empresa no backend.

---
+++ src/app/_services/company/company.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICompany } from 'src/app/_interfaces/company/icompany';
+import { API_BASE } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CompanyService {
+  constructor(private http: HttpClient) {}
+
+  saveCompany(company: ICompany): Observable<ICompany> {
+    return this.http.post<ICompany>(`${API_BASE}/company`, company);
+  }
+}
