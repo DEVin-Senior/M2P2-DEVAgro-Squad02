c6c460a7ff05ab7ffd7fce3616f154cb97ce88d8
Jordy Felipe da Silva <jordyf.silva@hotmail.com>
2022-06-11 13:28:36 -0300

    task35 - adicionada página home ao `app-routing.module`.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './_views/login/login.component';
 import { CompanyFormComponent } from './_views/company-form/company-form.component';
 import { FarmFormComponent } from './_components/farm/farm-form/farm-form.component';
 import { FarmListComponent } from './_components/farm/farm-list/farm-list.component';
+import { HomeComponent } from './_components/home/home/home.component';
 
 const routes: Routes = [
   { path: 'company/company-form', component: CompanyFormComponent },
   { path: 'login', component: LoginComponent },
   { path: "farm/farm-form", component: FarmFormComponent },
-  { path: "farm/farm-list", component: FarmListComponent }
+  { path: "farm/farm-list", component: FarmListComponent },
+  { path: "home", component: HomeComponent }
 ];
 
 @NgModule({
