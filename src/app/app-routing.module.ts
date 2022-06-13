e2ff8ffe53f2185ba555895bbbc4ec1e33f15728
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-06-13 20:30:57 -0300

    task33 - implementada funcionalidade para busca dos grãos da empresa logada.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './_views/login/login.component';
 import { CompanyFormComponent } from './_views/company-form/company-form.component';
@@ -6,12 +6,14 @@ import { FarmFormComponent } from './_components/farm/farm-form/farm-form.compon
 import { FarmListComponent } from './_components/farm/farm-list/farm-list.component';
 import { HomeComponent } from './_components/home/home/home.component';
 import { ShowWorkerComponent } from './_components/show-worker/show-worker.component';
+import { GrainsQueryComponent } from './_components/grain/grains-query/grains-query.component';
 
 const routes: Routes = [
   { path: 'company/company-form', component: CompanyFormComponent },
   { path: 'login', component: LoginComponent },
   { path: "farm/farm-form", component: FarmFormComponent },
   { path: "farm/farm-list", component: FarmListComponent },
+  { path: "grain/grain-list", component: GrainsQueryComponent},
   { path: "home", component: HomeComponent },
   { path: 'employee/list', component: ShowWorkerComponent }
 
